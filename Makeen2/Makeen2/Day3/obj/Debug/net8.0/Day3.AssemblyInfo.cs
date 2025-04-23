d7fd48cd45790d63671a5cf6431f95bae0dae5c0
Ibrahim-Al-falahi <the_punisher_oman@hotmail.com>
2025-04-23 17:57:56 +0400

    Day5 added

--- Makeen2/Makeen2/Day3/obj/Debug/net8.0/Day3.AssemblyInfo.cs
+++ Makeen2/Makeen2/Day3/obj/Debug/net8.0/Day3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96760292ef83d9098778fbb9b0a28417b62a7e4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5e9f8589c51e92b493c98a2f68ca8bd7971e57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Makeen2/Makeen2/Day5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, 1, 1, 1 }; // [0]=north , [1]=west , [2]=south , [3]=east
+            //int[] input = { 1, 2, 3, 4 }; //false
+            //int[] input = { 4, 3, 2, 1 }; //false
+            //int[] input = { 2, 1, 1, 2 }; //true
+
+
+            if (input[0] >= input[2] && input[1] <= input[3])
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+        }
+    }
+}
